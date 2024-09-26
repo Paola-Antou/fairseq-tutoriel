dbaabdf8b9b2554d626f1a3c6e1787d23751bb7d
Paola-Antou <119418463+Paola-Antou@users.noreply.github.com>
2024-09-26 07:46:49 +0100

    Update 4_⚙️Pré-traitement.py

--- pages/4_⚙️Pré-traitement.py
+++ pages/4_⚙️Pré-traitement.py
@@ -196,9 +196,9 @@ def afficher_architecture_concepts():
     # Affichage de la notification à la fin
     if st.button("Commencer l'exercice"):
         st.success("Bonne chance pour l'exercice !")
-        st.switch_page('pages/Evaluation.py')
-    if st.button("Pas maintenant"):
         st.switch_page('pages/5_Evaluation.py')
+    if st.button("Pas maintenant"):
+        st.switch_page('pages/6_⚡fine_tuning_optimisation.py')
 
     
 if __name__ == "__main__":
